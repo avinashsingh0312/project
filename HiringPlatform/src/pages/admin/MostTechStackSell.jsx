2486904051b59b7574731329c413e5487f9fca77
absharfatima <absharrabani1708@gmail.com>
2024-02-21 15:51:35 +0530

    Final commit

--- HiringPlatform/src/pages/admin/MostTechStackSell.jsx
+++ HiringPlatform/src/pages/admin/MostTechStackSell.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Pie } from "react-chartjs-2";
-//import Chart from "chart.js/auto";
+import Chart from "chart.js/auto";
 
 function MostTechStackSell() {
   const [technologyData, setTechnologyData] = useState([]);
